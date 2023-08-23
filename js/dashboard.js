3e4567bc18e8933de2f711f6ae373422d1edc16b
HECTOR NIEL PEREZ VALLADARES <16203442+manuelex20@users.noreply.github.com>
2023-08-22 20:28:09 -0500

    responsive

--- js/dashboard.js
+++ js/dashboard.js
@@ -45,3 +45,9 @@ menu_btn.addEventListener("click", () => {
     }
 
 });
+
+let navegador = navigator.userAgent;
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+    menu_btn.addEventListener('click', e => console.log("mobile"));
+    menu_btn.dispatchEvent(new Event('click'));
+} 
